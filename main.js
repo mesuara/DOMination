cad35551e447338ab35a062c7d28f1bba631c610
mesuara <mesuara@gmail.com>
2019-11-18 14:05:35 -0500

    added the sepiafilter

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function printImg(img, imgIndex) {
 
 
 
-
+  createNewDiv.appendChild(createDrop(imgElement))
    createNewDiv.appendChild(removeButton);
    createNewDiv.appendChild(editButton);
    createNewDiv.append(imgElement)
@@ -62,6 +62,7 @@ function printImg(img, imgIndex) {
   const editButton = document.createElement('button');
   editButton.className = 'editButton'
   editButton.innerText = 'edit'
+  createNewDiv.appendChild(createDrop(imgElement))
   createNewDiv.appendChild(removeButton);
   createNewDiv.appendChild(editButton);
   createNewDiv.append(imgElement)
@@ -135,214 +136,86 @@ function clearList() {
   }
 }
 
-// function markComplete(index) {
-//   // If it's complete in the data, set it to not complete, and vice versa!
-//   if  (images[index].completed === false) {
-//    images[index].completed = true;
-//   } else if  (images[index].completed === true) {
-//    images[index].completed = false;
-//   }
-// }
-
-// function printMark(index) {
-//   const li = document.querySelector(`li:nth-child(${index + 1})`);
-  
-//     // Set the dom styling appropriately here based on whether it's complete or not!
-//     if  (images[index].completed = true) {
-//       li.classList.toggle(`complete-item`);
-//     }
-//   }
-
-// const deleteButton = document.querySelector('.delete-completed images');
-// deleteButton.onclick = handleDelete;
-
-// function handleDelete() {
-//   const newTodos = [];
-
-//   // Only push into newTodos any todo that is NOT complete.
-//   for (i = 0; i < images.length; i ++) {
-//     if  (images[i].completed === false) {
-//       newTodos.push (images[i]);
-//     }
-//   }
-  
-//  images = newTodos;
-//   clearList();
-//   printList();
-// }
 
 
-// function clearList() {
-//   // Grab the todo list ul and put it in a variable
-//   const list = document.querySelector('.todo-list');
+function createDrop(img){
+//   <div class="dropdown">
+//   <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+//   <div id="myDropdown" class="dropdown-content">
+//     <a href="#home">Home</a>
+//     <a href="#about">About</a>
+//     <a href="#contact">Contact</a>
+//   </div>
+// </div>
+console.log(img)
+let mainDivDrop = document.createElement('div')
+mainDivDrop.className = 'dropdown'
+let button = document.createElement('button')
+button.className = 'dropbtn'
+button.innerText = 'Filter'
+button.addEventListener('click',dropFunction)
+mainDivDrop.append(button)
+let dropDiv = document.createElement('div')
+dropDiv.className = 'dropdown-content'
+dropDiv.id = 'myDropdown'
+mainDivDrop.append(dropDiv)
+let sepiaFilter  = document.createElement('a')
+sepiaFilter.innerText = 'sepia'
+sepiaFilter.className = 'sepia'
+sepiaFilter.addEventListener('click', function(){
+  addImgFilter(img, 'sepia')
+})
+dropDiv.append(sepiaFilter)
 
-//   // Remove all children of that list.
-//   // My favorite way uses `.hasChildNodes()` and `.remove()` and `.firstChild`, but there are other ways if you wanna research them instead!
-//   while (list.hasChildNodes()) {
-//     list.firstChild.remove();
-//   }
-// }
+let contrastFilter  = document.createElement('a')
+contrastFilter.innerText = 'contrast'
+contrastFilter.className = 'contrast'
+contrastFilter.addEventListener('click', function(){
+  addImgFilter(img, 'contrast')
+})
+dropDiv.append(contrastFilter)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let inputField = document.querySelector('#inputDiv')
-// let addNewButton = document.querySelector('#addNew')
-// let addButton = document.querySelector('#add')
-// let mainDiv = document.querySelector('#mainDiv')
-// inputField.style.display = 'none'
-
-// addNewButton.addEventListener("click", showDivInput);
-// addButton.addEventListener("click", addImageSrc);
-// //showing or hiding the div that contains the input files when you want to add a new image
-// function showDivInput(){
-//   if(inputField.style.display === 'none'){
-//     document.querySelector('.inputForNew').value = ' '
-//     inputField.style.display = 'flex'
-//   }else{
-//     inputField.style.display = 'none'
-//   }
-// }
+return mainDivDrop
+}
 
 
-// let allImg = document.querySelectorAll('img')
-// function addDivsAndButton() {
-//   for(let i=0; i < allImg.length; i++){
-//     //going through all the images 
-//    let parentDiv = allImg[i].parentElement.id
-//     console.log(parentDiv)
-//     //grabbing the current div
-//     let actualDiv = document.querySelector(`#${parentDiv}`)
-//     //creating the edit button
-//     let editButton = document.createElement('button')
-//     editButton.id = 'editButton'
-//     editButton.innerText = 'edit'
-//     //creating the remove button
-//     let removeButton = document.createElement('button')
-//     removeButton.id = 'removeButton'
-//     removeButton.innerText = 'remove'
-    
-//     //creating a div to append it to the parent div of the image and also it helps keep the components nice 
-//     let wrapDiv = document.createElement('div')
-//     wrapDiv.className = `imgDiv class${i}`
-//     wrapDiv.id = 'div'+i
-//     //appending the created div to the main div and the button to the div we created
-   
-//     wrapDiv.append(editButton)
-//     wrapDiv.append(removeButton)
-//     wrapDiv.append(allImg[i])
-//     actualDiv.append(wrapDiv)
-//     removeButton.addEventListener("click", function() {
-//       removeImg(wrapDiv);
+function dropFunction(){
+  document.getElementById("myDropdown").classList.toggle("show")
+}
 
-//   })
-//   }
-//   return allImg
-// }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 
-// function addDivsAndButtonForNewImg(img){
-//   console.log(img)
-//   let parentDiv = img.parentElement.id
-//     console.log(parentDiv)
-//     //grabbing the current div
-//     let actualDiv = document.querySelector(`#${parentDiv}`)
-//     //creating the edit button
-//     let editButton = document.createElement('button')
-//     editButton.id = 'editButton'
-//     editButton.innerText = 'edit'
-//     //creating the remove button
-//     let removeButton = document.createElement('button')
-//     removeButton.id = 'removeButton'
-//     removeButton.innerText = 'remove'
-    
-//     //creating a div to append it to the parent div of the image and also it helps keep the components nice 
-//     let wrapDiv = document.createElement('div')
-//     wrapDiv.className = `imgDiv class${i}`
-//     wrapDiv.id = 'div'+i
-//     //appending the created div to the main div and the button to the div we created
-   
-//     wrapDiv.append(editButton)
-//     wrapDiv.append(removeButton)
-//     wrapDiv.append(allImg[i])
-//     actualDiv.append(wrapDiv)
-//     removeButton.addEventListener("click", function() {
-//       removeImg(wrapDiv);
-
-// })
-
-// }
-// //calling the function so it always shows
+function addImgFilter(img,filter){
+  console.log(img)
+  if(filter === 'sepia'){
+  img.style.filter = "sepia()"
+  }
+  if(filter === 'contrast'){
+    img.style.filter = "contrast()"
+    }
+}
 
 
 
-// //grabing the source of the image on add new image from the input field and adding it to the page
-// function addImageSrc() {
-//   //creating the new img element for the new image
-//   let newImg = document.createElement('img')
-//   newImg.className = allImg.length + 1
-//   //grabing the input calue the url in this case
-//   newImg.src = document.querySelector('.inputForNew').value
-//   //getting all the divs in order to grab the last div later
-//   let howManyDivs = document.querySelectorAll('div')
 
-//   console.log(howManyDivs)
-//   let lastDiv = howManyDivs[howManyDivs.length -1]
-// console.log('this is div count' + howManyDivs.length)
-//   lastDiv.className = `last-${howManyDivs.length} last`
-  
-// //grabing the image from the last div 
-//   let howManyImg = document.querySelectorAll(`.last-${howManyDivs.length} img`)
-//   let imgNum = howManyImg.length
-//   console.log(imgNum)
-//   //checking if there are 3 images already so I can create a new div and I keep tha layout 3 images in a row
-//   if(imgNum >= 3 ){
-//     let newDiv= document.createElement('div')
-//     newDiv.className = `last-${howManyDivs.length +1}`
-//     newDiv.id =`${howManyDivs.length}`
-//     mainDiv.append(newDiv)
-    
-//     showDivInput()
-//    newDiv.append(newImg)
-//   return addDivsAndButtonForNewImg(newImg)
-//   }else{
-//     addDivsAndButtonForNewImg(newImg)
-//     showDivInput()
-//     lastDiv.append(newImg)
-//     return addDivsAndButtonForNewImg(newImg)
 
-//   }
 
-// }
 
-// //remove img function 
 
-// function removeImg(element){
-//   console.log('inside the remove function')
-//   console.log(element.parentNode)
-//   element.parentNode.removeChild(element)
-//   return element.remove()
-// }
 
-// addDivsAndButton()
+
+
+
+
